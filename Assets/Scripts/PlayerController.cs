971c2d5fb4647676b80733864fc4841b92b3b4a7
Fabián E. García <fabianegl79@gmail.com>
2024-03-01 14:17:24 -0300

    mover al player con RigidBody y evitar que el auto voltee

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float horsePower = 0; // caballos de fuerza
     [SerializeField] private float turnSpeed = 45.0f;
+    [SerializeField] GameObject centerOfMass; 
     private float horizontalInput;
     private float verticalInput;
+    private Rigidbody playerRb;
 
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        
+    }
+
+    private void Start()
+    {
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         //acá tomamos el input del player
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        
+
         // acá movemos el player (Time.deltatime cambia la frecuencia del update en segundos y no frames o cuadros)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
 
         // acá giramos el player 
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
